568bb6339fdc6f04849eb2e90e8f79c60fe98be7
Petr Grishin <petr.grishin@grishini.ru>
2014-06-07 00:02:54 +0400

    Рендер. Реализованы методы работы с путями шаблона

--- src/ViewRenderer.php
+++ src/ViewRenderer.php
@@ -11,14 +11,25 @@ use IViewRenderer;
 
 class ViewRenderer extends CApplicationComponent implements IViewRenderer {
     public $fileExtension = '.php';
+    public $fileExtensionJs = '.js';
 
     public static function className() {
         return get_called_class();
     }
 
-    public function renderFile($context, $sourceFile, $data, $return) {
-        $view = new View();
-        $view->setParams($data);
-        return $view->render($sourceFile, $return);
+    public function renderFile($context, $sourceFile, $params, $isReturn) {
+        $view = new View($context);
+        $view->setParams($params);
+        $response = $view->render($sourceFile, $isReturn);
+
+        return $response;
+    }
+
+    public function getJsFile($sourceFile) {
+        return $this->getBaseFilename($sourceFile) . $this->fileExtensionJs;
+    }
+
+    public function getBaseFilename($sourceFile) {
+        return substr($sourceFile, 0, - strlen($this->fileExtension));
     }
 }
