e67e9bb7a76cfe0a968e8e51dae54384f057d351
Petr Grishin <petr.grishin@grishini.ru>
2014-06-12 17:05:44 +0400

    Рефакторинг механизма регистрации и вызова скриптов

--- src/ViewScriptProcessor.php
+++ src/ViewScriptProcessor.php
@@ -19,18 +19,29 @@ class ViewScriptProcessor extends \CApplicationComponent {
         foreach (array_reverse($view->getWidgets()) as $widgetClass => $widgets) {
             /** @var \PetrGrishin\View\Widget $widget */
             foreach ($widgets as $widget) {
-                $this->appendScriptFile($widget->getView()->getScriptFile());
-                printf("App.register('%s', '%s');\n", $widget->getView()->getId(), $widget->getView()->getScriptFile());
+                $this->appendScriptFile($widget->getView()->getId(), $widget->getView()->getScriptFile());
                 $widgetsIds[$widget->getName()] = $widget->getView()->getId();
             }
         }
-        printf("App.register('%s', '%s');\n", $view->getId(), $view->getScriptFile());
-        printf("App.do('%s', %s);\n", $view->getId(), json_encode($widgetsIds));
+        $isAppend = $this->appendScriptFile($view->getId(), $view->getScriptFile());
+        $isAppend && $this->runScript($view->getId(), $widgetsIds);
     }
 
-    public function appendScriptFile($fileScript) {
-        $this->getClientScript()->registerScriptFile($fileScript, CClientScript::POS_HEAD);
-        return $this;
+    public function appendScriptFile($id, $fileScript) {
+        if (!file_exists($fileScript)) {
+            return false;
+        }
+        $script = trim(file_get_contents($fileScript));
+        if (!$script) {
+            return false;
+        }
+        $this->getClientScript()->registerScript($id . '_append', sprintf("App.register('%s', %s);", $id, $script), CClientScript::POS_HEAD);
+        return true;
+    }
+
+    public function runScript($id, $widgetsIds) {
+        $script = sprintf("App.run('%s', %s);", $id, json_encode($widgetsIds));
+        $this->getClientScript()->registerScript($id . '_run', $script, CClientScript::POS_HEAD);
     }
 
     /**
---
+++ tests/views/test/test.js
@@ -0,0 +1,5 @@
+(function() {
+    return function (params, scope, widgets) {
+
+    };
+})()
